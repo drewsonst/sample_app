d79c619e0fb3afef37e71d024cb70858cc00127a
drewsonst <drew.sonst@gmail.com>
2014-12-19 01:55:49 +0000

    End chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #Returns the full title on a per page basis
+    def full_title(page_title = '') #if nothing passed in set to empty?
+       base_title = "Ruby on Rails Sample App"
+       if page_title.empty?
+           base_title
+        else
+            "#{page_title} | #{base_title}" #ex Home | Ruby on Rails Sample App
+        end
+    end
 end
